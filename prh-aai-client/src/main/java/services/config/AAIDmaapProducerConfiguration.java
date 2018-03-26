b80bcb81356fc9953e85196d8cbf6ada2ebbc143
wasala <przemyslaw.wasala@nokia.com>
2018-03-26 15:48:46 +0200

    Divide into module dependecy
    Add dependency build of dmaapClients and aaiClients.
    This modules are mandatory to prh application.
    
    Issue-ID: DCAEGEN2-407
    
    Change-Id: I2150a15d76e81dd01503f44339ddb3c50f5b0348
    Signed-off-by: wasala <przemyslaw.wasala@nokia.com>

---
+++ prh-aai-client/src/main/java/services/config/AAIDmaapProducerConfiguration.java
@@ -0,0 +1,41 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * PROJECT
+ * ================================================================================
+ * Copyright (C) 2018 NOKIA Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package services.config;
+
+import java.net.URL;
+import org.immutables.value.Value;
+
+/**
+ * @author <a href="mailto:przemyslaw.wasala@nokia.com">Przemysław Wąsala</a> on 3/23/18
+ */
+@Value.Immutable(prehash = true)
+@Value.Style(stagedBuilder = true)
+public abstract class AAIDmaapProducerConfiguration extends DmaapConfig {
+
+    private static final long serialVersionUID = 1L;
+
+    private String aaiHost;
+    private Integer aaiHostPortNumber;
+    private String aaiProtocol;
+    private String aaiUserName;
+    private String aaiUserPassword;
+    private URL aaiProxyURL;
+    private boolean aaiIgnoreSSLCertificateErrors;
+}
---
+++ prh-app-server/src/main/java/org/onap/dcaegen2/services/prh/tasks/DmaapConsumerTask.java
@@ -0,0 +1,46 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * PROJECT
+ * ================================================================================
+ * Copyright (C) 2018 NOKIA Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.dcaegen2.services.prh.tasks;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author <a href="mailto:przemyslaw.wasala@nokia.com">Przemysław Wąsala</a> on 3/23/18
+ */
+@Component
+public class DmaapConsumerTask implements DmaapTask {
+
+    private static final Logger logger = LoggerFactory.getLogger(DmaapConsumerTask.class);
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    @Override
+    public void execute() {
+        logger.info("Start task DmaapConsumerTask::execute() :: Execution Time - {}", dateTimeFormatter.format(
+            LocalDateTime.now()));
+        //TODO: ADD implementation for executing request to consume topic from dmaap
+
+        logger.info("End task DmaapConsumerTask::execute() :: Execution Time - {}",
+            dateTimeFormatter.format(LocalDateTime.now()));
+    }
+}

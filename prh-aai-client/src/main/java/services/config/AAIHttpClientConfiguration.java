2363225d6f7243e3add8c5f7275067833a6b3405
pwielebs <piotr.wielebski@nokia.com>
2018-03-27 19:49:29 +0200

    AAI HTTP simple and extended clients added
    Change-Id: I759aeeb64085dbe32883a942941bffc8e03515f7
    Issue-ID: DCAEGEN2-407
    Signed-off-by: pwielebs <piotr.wielebski@nokia.com>

---
+++ prh-aai-client/src/main/java/services/config/AAIHttpClientConfiguration.java
@@ -0,0 +1,52 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * PNF-REGISTRATION-HANDLER
+ * ================================================================================
+ * Copyright (C) 2018 NOKIA Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package services.config;
+
+
+import org.immutables.value.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Value.Immutable(prehash = true)
+@Value.Style(stagedBuilder = true)
+public abstract class AAIHttpClientConfiguration implements AAIConfig {
+
+    private static final long serialVersionUID = 1L;
+
+    @Value.Parameter
+    public abstract String aaiHost();
+
+    @Value.Parameter
+    public abstract Integer aaiHostPortNumber();
+
+    @Value.Parameter
+    public abstract String aaiProtocol();
+
+    @Value.Parameter
+    public abstract String aaiUserName();
+
+    @Value.Parameter
+    public abstract String aaiUserPassword();
+
+    @Value.Parameter
+    public abstract boolean aaiIgnoreSSLCertificateErrors();
+
+}

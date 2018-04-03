d7e240a5786fbd60eddc2634150c4cfbb7b8c9e6
wasala <przemyslaw.wasala@nokia.com>
2018-04-03 14:41:23 +0200

    Added pushing docker image to nexus
    Extended appConf loading
    
    Change-Id: Ibc3640cf4a66e40ce5dc585818c077534d09141c
    Issue-ID: DCAEGEN2-407
    Signed-off-by: wasala <przemyslaw.wasala@nokia.com>

---
+++ prh-app-server/src/main/java/org/onap/dcaegen2/services/prh/config/ApplicationProperties.java
@@ -0,0 +1,8 @@
+package org.onap.dcaegen2.services.prh.config;
+
+/**
+ * @author <a href="mailto:przemyslaw.wasala@nokia.com">Przemysław Wąsala</a> on 4/3/18
+ */
+public class ApplicationProperties {
+
+}

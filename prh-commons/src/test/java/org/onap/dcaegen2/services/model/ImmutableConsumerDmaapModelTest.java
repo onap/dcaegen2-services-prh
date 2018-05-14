7f0ca03baae326fe37868e1518183422c40f2225
pwielebs <piotr.wielebski@nokia.com>
2018-05-14 16:52:05 +0200

    Unit class added
    Change-Id: I7a11d1ff3aa38b6262635eead40cb737905de558
    Issue-ID: DCAEGEN2-451
    Signed-off-by: pwielebs <piotr.wielebski@nokia.com>

--- prh-commons/src/test/java/org/onap/dcaegen2/services/model/ImmutableConsumerDmaapModelTest.java
+++ prh-commons/src/test/java/org/onap/dcaegen2/services/model/ImmutableConsumerDmaapModelTest.java
@@ -20,23 +20,31 @@
 
 package org.onap.dcaegen2.services.model;
 
-public class ConsumerDmaapModelForUnitTest implements ConsumerDmaapModel {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class ImmutableConsumerDmaapModelTest {
+
+    // Given
+    private ConsumerDmaapModel consumerDmaapModel;
     private String pnfName = "NOKnhfsadhff";
     private String ipv4 = "11.22.33.155";
     private String ipv6 = "2001:0db8:85a3:0000:0000:8a2e:0370:7334";
 
+    @Test
+    public void consumerDmaapModelBuilder_shouldBuildAnObject() {
 
-    public String getPnfName() {
-        return pnfName;
+        // When
+        consumerDmaapModel = ImmutableConsumerDmaapModel.builder()
+                .pnfName(pnfName)
+                .ipv4(ipv4)
+                .ipv6(ipv6)
+                .build();
+
+        // Then
+        Assertions.assertNotNull(consumerDmaapModel);
+        Assertions.assertEquals(pnfName,consumerDmaapModel.getPnfName());
+        Assertions.assertEquals(ipv4,consumerDmaapModel.getIpv4());
+        Assertions.assertEquals(ipv6,consumerDmaapModel.getIpv6());
     }
-
-    public String getIpv4() {
-        return ipv4;
-    }
-
-    public String getIpv6() {
-        return ipv6;
-    }
-
 }

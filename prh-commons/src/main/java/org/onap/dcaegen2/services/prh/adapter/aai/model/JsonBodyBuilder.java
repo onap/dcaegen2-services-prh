9c9d2523c942018ae39bf00d12ffb3b1a3ea1530
Michal Kabaj <michal.kabaj@nokia.com>
2019-12-03 12:38:20 +0100

    Refactor AaiClient API - Removed unused AaiHttpPutClient - Removed unnecessary ClientModel and DmaapModel interfaces
    Issue-ID: DCAEGEN2-1955
    Change-Id: Id95dd4819b7fada634ac6dace02a3e27219aacdd
    Signed-off-by: Michal Kabaj <michal.kabaj@nokia.com>

--- prh-commons/src/main/java/org/onap/dcaegen2/services/prh/adapter/aai/model/JsonBodyBuilder.java
+++ prh-commons/src/main/java/org/onap/dcaegen2/services/prh/adapter/aai/model/JsonBodyBuilder.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * DCAEGEN2-SERVICES-SDK
  * ================================================================================
- * Copyright (C) 2018 NOKIA Intellectual Property. All rights reserved.
+ * Copyright (C) 2019 NOKIA Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@
 package org.onap.dcaegen2.services.prh.adapter.aai.model;
 
 @FunctionalInterface
-public interface JsonBodyBuilder<T extends ClientModel> {
+public interface JsonBodyBuilder<T> {
 
     /**
      * Method for serialization object by GSON.
